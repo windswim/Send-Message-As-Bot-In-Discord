b9ce6eaad0792f1963bce75bc35f6ae2171c9ee9
GameOverAgain <nusret.balacayev@gmail.com>
2021-07-12 20:48:37 +0400

    create

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.send_message = new System.Windows.Forms.TextBox();
+            this.send_bt = new System.Windows.Forms.Button();
+            this.console_output_tb = new System.Windows.Forms.TextBox();
+            this.token_tb = new System.Windows.Forms.TextBox();
+            this.channeli_id_tb = new System.Windows.Forms.TextBox();
+            this.connect_bt = new System.Windows.Forms.Button();
+            this.messages_tb = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // send_message
+            // 
+            this.send_message.Location = new System.Drawing.Point(12, 228);
+            this.send_message.Multiline = true;
+            this.send_message.Name = "send_message";
+            this.send_message.Size = new System.Drawing.Size(359, 82);
+            this.send_message.TabIndex = 0;
+            this.send_message.Text = "Text to send";
+            this.send_message.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // send_bt
+            // 
+            this.send_bt.Location = new System.Drawing.Point(12, 316);
+            this.send_bt.Name = "send_bt";
+            this.send_bt.Size = new System.Drawing.Size(179, 46);
+            this.send_bt.TabIndex = 1;
+            this.send_bt.Text = "Send";
+            this.send_bt.UseVisualStyleBackColor = true;
+            this.send_bt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // console_output_tb
+            // 
+            this.console_output_tb.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.console_output_tb.Location = new System.Drawing.Point(382, 12);
+            this.console_output_tb.Multiline = true;
+            this.console_output_tb.Name = "console_output_tb";
+            this.console_output_tb.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.console_output_tb.Size = new System.Drawing.Size(190, 298);
+            this.console_output_tb.TabIndex = 2;
+            this.console_output_tb.Text = "Output of bot";
+            // 
+            // token_tb
+            // 
+            this.token_tb.Location = new System.Drawing.Point(197, 316);
+            this.token_tb.Name = "token_tb";
+            this.token_tb.Size = new System.Drawing.Size(174, 20);
+            this.token_tb.TabIndex = 3;
+            this.token_tb.Text = "Token";
+            this.token_tb.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // channeli_id_tb
+            // 
+            this.channeli_id_tb.Location = new System.Drawing.Point(197, 342);
+            this.channeli_id_tb.Name = "channeli_id_tb";
+            this.channeli_id_tb.Size = new System.Drawing.Size(174, 20);
+            this.channeli_id_tb.TabIndex = 4;
+            this.channeli_id_tb.Text = "Channel id";
+            this.channeli_id_tb.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // connect_bt
+            // 
+            this.connect_bt.Location = new System.Drawing.Point(382, 314);
+            this.connect_bt.Name = "connect_bt";
+            this.connect_bt.Size = new System.Drawing.Size(190, 48);
+            this.connect_bt.TabIndex = 5;
+            this.connect_bt.Text = "Connect";
+            this.connect_bt.UseVisualStyleBackColor = true;
+            this.connect_bt.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // messages_tb
+            // 
+            this.messages_tb.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.messages_tb.Location = new System.Drawing.Point(12, 12);
+            this.messages_tb.Multiline = true;
+            this.messages_tb.Name = "messages_tb";
+            this.messages_tb.ReadOnly = true;
+            this.messages_tb.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.messages_tb.Size = new System.Drawing.Size(359, 210);
+            this.messages_tb.TabIndex = 6;
+            this.messages_tb.TabStop = false;
+            this.messages_tb.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 366);
+            this.Controls.Add(this.messages_tb);
+            this.Controls.Add(this.connect_bt);
+            this.Controls.Add(this.channeli_id_tb);
+            this.Controls.Add(this.token_tb);
+            this.Controls.Add(this.console_output_tb);
+            this.Controls.Add(this.send_bt);
+            this.Controls.Add(this.send_message);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Discord Bot login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox send_message;
+        private System.Windows.Forms.Button send_bt;
+        private System.Windows.Forms.TextBox console_output_tb;
+        private System.Windows.Forms.TextBox token_tb;
+        private System.Windows.Forms.TextBox channeli_id_tb;
+        private System.Windows.Forms.Button connect_bt;
+        private System.Windows.Forms.TextBox messages_tb;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Discord;
+using Discord.WebSocket;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        DiscordSocketClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            ulong id = Convert.ToUInt64(channeli_id_tb.Text);
+            var chnl = client.GetChannel(id) as IMessageChannel;
+            string msg = send_message.Text;
+            chnl.SendMessageAsync(msg);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            client.Log += Log;
+            client.MessageReceived += CommandsHaddler;
+
+            try
+            {
+                await client.LoginAsync(TokenType.Bot, token_tb.Text);
+                await client.StartAsync();
+            }
+            catch
+            {
+                MessageBox.Show("Error occured while connection your bot", "ERROR");
+                return;
+            }
+            await Task.Delay(1000);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Invoke((Action)delegate
+            {
+                console_output_tb.AppendText(arg + "\n");
+            });
+            return null;
+        }
+        public Task CommandsHaddler(SocketMessage msg)
+        {
+            Invoke((Action)delegate
+            {
+            string texttomsg = msg.Content.ToString();
+             messages_tb.AppendText($"[{msg.Author.ToString()}]: " + texttomsg + "\r\n");
+            });
+            return null;
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
